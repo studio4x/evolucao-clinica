b14322707cc342ecef9e921feeef73f0f4ce19f4
Studio4x AI <contato@studio4x.com.br>
2026-07-10 14:54:22 -0300

    chore(seo): change fallback og:image to absolute url in index.html to satisfy facebook debugger

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const APP_VERSION = "v1.10.344";
+export const APP_VERSION = "v1.10.346";
 export const PLAY_STORE_VERSION = "1.0.2";
 
 interface AppVersionProps {
