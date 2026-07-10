ada34bcd4257eea059025194d58ba2468cdf7228
Studio4x AI <contato@studio4x.com.br>
2026-07-10 14:49:49 -0300

    chore(seo): change fallback og:image to og-image-social.png in index.html

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const APP_VERSION = "v1.10.343";
+export const APP_VERSION = "v1.10.344";
 export const PLAY_STORE_VERSION = "1.0.2";
 
 interface AppVersionProps {
