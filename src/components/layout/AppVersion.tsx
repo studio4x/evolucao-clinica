b930e4c826e1cc2fa4448d154f8a84a4da21fcea
Studio4x AI <contato@studio4x.com.br>
2026-07-14 16:09:44 -0300

    feat: cadastro dos 15 dias da Jornada no banco de dados e migracao do sistema de jornada - build v1.10.412

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const APP_VERSION = "v1.10.411";
+export const APP_VERSION = "v1.10.412";
 export const PLAY_STORE_VERSION = "1.0.11";
 
 interface AppVersionProps {
