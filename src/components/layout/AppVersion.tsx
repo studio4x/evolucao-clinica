6320dc43c8276b87cc7da08d3813ac808d251df2
Studio4x AI <contato@studio4x.com.br>
2026-07-22 09:08:20 -0300

    build(android): prepare FCM release 59

--- src/components/layout/AppVersion.tsx
+++ src/components/layout/AppVersion.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const APP_VERSION = "v1.10.612";
+export const APP_VERSION = "v1.10.613";
 
-export const PLAY_STORE_VERSION = "1.0.58";
+export const PLAY_STORE_VERSION = "1.0.59";
 
 interface AppVersionProps {
   collapsed?: boolean;
